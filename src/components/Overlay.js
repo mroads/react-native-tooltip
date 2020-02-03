833fd07a2d3a5ec803f053d8ae139b987c5e234f
snigdha.dhanireddy@mroads.com-at-669308558862 <snigdha.dhanireddy@mroads.com>
2020-02-03 16:06:17 +0530

    Removing inner container

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -13,12 +13,7 @@ const styles = StyleSheet.create({
     height: '100%',
     position: 'absolute',
     zIndex: 2,
-  },
-  innerContainer: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
+  }
 });
 
 export interface Props {
